5769bec1a89c0a02c4f5058c353d516d90e0145f
ggualbertosouza <contatoguilhermegsz@gmail.com>
2023-06-21 00:07:25 -0300

    finishing the project

--- programs/loja_livros.py
+++ programs/loja_livros.py
@@ -6,7 +6,6 @@ def main():
     count = 0
 
     livros = []
-
     while count != 4:
         print('===== Minha estante de livros =====')
         print('\nAqui você guarda alguns livros que já leu e mostra para o mundo.\n')
@@ -15,27 +14,43 @@ def main():
         print('3 - Ver toda a estante')
         print('4 - Sair\n')
 
-        escolha = int(input(''))
+        count = int(input(''))
 
-        if escolha == 1:
+        if count == 1:
             os.system('cls')
 
             print('====== Adicione um novo livro =====\n')
             nomeLivro = input('Nome do livro: ')
             autor = input('Nome do autor: ')
             status = input('Já leu ou não?')
-
             livros.append([nomeLivro, autor, status])
 
-        elif escolha == 2:
+        elif count == 2:
             os.system('cls')
 
-            i = input('Pesquisa: ')
+            escolha = input('Pesquisa: ')
             for livro in livros:
-                if i in livro:
+                if escolha in livro:
                     print(livro)
-        elif escolha == 3:
-            pass
+
+        elif count == 3:
+            os.system('cls')
+            print('===== Aqui estão todos os seus livros =====')
+            lib = open('Books.txt', 'r')
+            for livro in livros:
+                print(lib.read(livro))
+            lib.close()
+
+        elif count == 4:
+            os.system('cls')
+            print('Até a próxima! ')
+
+    # Salvando em arquivo externo
+
+    escrita = open('Books.txt', 'w')
+    for livro in livros:
+        escrita.write(','.join(livro) + '\n')
+    escrita.close()
 
 
 main()
